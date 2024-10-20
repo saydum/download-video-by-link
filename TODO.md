908f3bf45701d5a174f78515524cbb36daefaf07
saydum <saydum.it5@gmail.com>
2024-10-20 20:51:20 +0300

    feat: implement download video

---
+++ TODO.md
@@ -0,0 +1,27 @@
+# Задача
+Создать приложение для скачивания видое по сылке
+
+## MVP
+- Скачать видео по сылке из YouTube
+- GUI
+  - Вставить сылку
+  - Кнопка скачать
+  - Прогресс скачивания
+  - Выаод завершения
+
+
+### Tasks
+- [x] create project
+
+- [ ] frontend
+  - [x] input link
+  - [x] btn download
+  - [ ] progress
+  - [ ] get message done
+
+- [ ] backend
+  - [x] set url
+  - [x] download video
+  - [ ] get progress
+  - [ ] return message
+  
--- main.py
+++ main.py
@@ -1,8 +1,49 @@
+import yt_dlp
 import flet as ft
 
+def download_video(url, progress_callback):
+    
+    def hook(d):
+        if d['status'] == 'downloading':
+            progress_callback(d['downloaded_bytes'], d['total_bytes'], d['filename'])
+            
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'progress_hooks': [hook],
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.title = "Download view by link v0.2.0"
+    
+    out_txt = ft.Text()
+    download_btn = ft.ElevatedButton(text="Скачать видео", on_click=lambda e: start_download())
+    input_link = ft.TextField(label="Вставте сылку из YouTube", width=400)
+    
+    def start_download():
+        url = input_link.value
+        if url:
+            out_txt.value = "Загрузка видео..."
+            page.update()
+            download_video(url, update_progress)
+    
+    def update_progress(downloaded_bytes, total_bytes, filename):
+        if total_bytes > 0:
+            percentage = (downloaded_bytes / total_bytes) * 100
+            out_txt.value = f"Загрузка {filename}: {downloaded_bytes} из {total_bytes} байт ({percentage:.2f}%)"
+            page.update() 
+    
+    # View
+    page.add(
+        ft.Row([ft.Text(page.title)], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([input_link, download_btn]),
+        ft.Row([out_txt])
+    )    
 
 
 ft.app(main)
